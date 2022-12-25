bbee458bee7263dfaf65f8c92d6c874c019bf2d4
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2022-12-25 06:47:52 -0300

    Initial

---
+++ VShop.ProductApi/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace VShop.ProductApi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+
+    //Relation 1 : M
+    public ICollection<Product>? Products { get; set; }
+}
